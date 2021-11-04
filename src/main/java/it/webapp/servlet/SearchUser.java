3244185767d076790e8a83e0e71145ed368d1a1d
Moa <infoerraji96@gmail.com>
2021-11-04 12:44:41 +0100

    Versione Mohamed e Andrea per l'inserimento in fase di sviluppo

---
+++ src/main/java/it/webapp/servlet/SearchUser.java
@@ -0,0 +1,9 @@
+package it.webapp.servlet;
+//Mohamed ed Andrea: tool di ricerca
+public class SearchUser {
+
+}
+//funziona? ottimo così almeno vediamo in diretta il codice senza fare mille update
+
+
+//pare di sì sopra vedo pure la funzione di chiamata
